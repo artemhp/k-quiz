9b437c49d871307368e1cf2b3c2fab6911cf81a2
KlaudijaDemchyk <fkst@i.ua>
2023-03-07 23:34:17 +0100

    init3

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 function check()
-{   var yes =0;
-    var no =0;
-    var otvet="";
-    var choice;
-    for (var v=1; v<=9; v++)
-    {   var q = document.getElementsByName('answer'+v)
-        for (var i=0; i<q.length; i++)
-        {
-            if (q[i].checked) {
-                choice=q[i].value;
+{   let yes = 0;
+    let no = 0;
+    let otvet = "";
+    let choice;
+    for (let v=1; v<=9; v++)
+    {
+        const q = document.getElementsByName('answer'+v)
+        q.forEach(element => {
+            if (element.checked) {
+                choice = element.value;
             }
-        }
+        });
         if (choice=="yes") {yes++;}
         if (choice=="no") {no++;}
     }
-    switch (true) {
-        case (yes<4): otvet="Your knowledge is not enough";break;
-        case (yes==6): otvet="Intermediate level of knowledge";break;
-        case (yes>8): otvet="Great job!You passed the test!";break;
-        default: otvet="Very Bad";break;
+    switch (true)
+    {
+        case (yes<4): otvet ="Your knowledge is not enough";break;
+        case (yes==6): otvet ="Intermediate level of knowledge";break;
+        case (yes>8): otvet ="Great job!You passed the test!";break;
+        default: otvet ="Very Bad";break;
     }
-    document.getElementById('Result').parentElement.insertAdjacentHTML('afterend', `<footer class="footer hero is-warning">
-  <div class="content has-text-centered">
-    <p>
+    document.getElementById('Result').parentElement.insertAdjacentHTML('afterend',
+        `<footer class="footer hero is-warning">
+            <div class="content has-text-centered">
+        <p>
       <strong>${otvet}</strong> 
-    </p>
-  </div>
-</footer>`);
+        </p>
+        </div>
+    </footer>`);
 }
