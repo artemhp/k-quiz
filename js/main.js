598cb275df99a63d0a58574b160f0d0fb97223be
KlaudijaDemchyk <fkst@i.ua>
2023-03-26 21:41:48 +0200

    Add function displayCards and iterateCards add array data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,87 @@
+const questions = [
+    {
+        question: " Explain how this works in JavaScript. Can you give an example of one of the ways that working with this has changed in ES6?",
+
+    },
+    {
+        question: " What's the difference between a variable that is: null, undefined or undeclared? How would you go about checking for any of these states?",
+    },
+    {
+        question: " Can you describe the main difference between the Array.forEach() loop and Array.map() methods and why you would pick one versus the other?",
+    },
+    {
+        question: " Can you offer a use case for the new arrow => function syntax? How does this new syntax differ from other functions?",
+    },
+    {
+        question: " What are some of the advantages/disadvantages of writing JavaScript code in a language that compiles to JavaScript?",
+    },
+    {
+        question: " What is the difference between while and do-while loops in JavaScript?",
+    },
+    {
+        question: " Can you give an example of a curry function and why this syntax offers an advantage?",
+    },
+    {
+        question: " Discuss how you might use Object Oriented Programming principles when coding with JavaScript.",
+    },
+    {
+        question: " What are the benefits of using spread syntax and how is it different from rest syntax?",
+    },
+];
+const cardsElement = document.getElementById("cards");
+let cardsHTML = "";
+
+function iterateCards(element, index) {
+    return `
+    <div class="card mt-2 ">
+      <div class="card-content">
+        <label class="checkbox">
+          <input type="checkbox" id="toggle-${index}" onclick="hideDiv('card-${index}')">
+          Hide the question
+        </label>
+        <div class="content" id="card-${index}">
+          ${element.question}
+          <div class="control">
+            <label class="radio">
+              <input type="radio" name="answer${index}" value="yes">
+              Yes
+            </label>
+            <label class="radio">
+              <input type="radio" name="answer${index}" value="no">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+
+function displayCards() {
+    let rowHTML = "";
+    let cardCount = 0; // Count of cards added to current row
+    questions.forEach((element, index) => {
+        const cardHTML = iterateCards(element, index + 1);
+        if (cardCount === 0) {
+            // Start a new row
+            rowHTML += '<section class="section"><div class="columns is-block">';
+        }
+        rowHTML += cardHTML;
+        cardCount++;
+        if (cardCount === 3 || index === questions.length - 1) {
+            // End the row
+            rowHTML += '</div></section>';
+            cardCount = 0;
+        }
+    });
+    cardsElement.innerHTML = rowHTML;
+}
+
+
+
+
+
 function check(v) {
     const q = document.getElementsByName('answer' + v);
     let result = null;
@@ -9,6 +93,7 @@ function check(v) {
     return result;
 }
 
+displayCards();
 function levelOfConfidence(yes) {
     if (yes < 4) {
         return "Your knowledge is not enough";
@@ -50,10 +135,9 @@ function add_Question() {
     </section>`
     );
 }
-
 function Result() {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    if (add_Question) {
+    if (document.getElementById('new_question').value) {
         arr.push('10');
     }
     let res = [];
@@ -81,6 +165,7 @@ function Result() {
     </footer>`
     );
 }
+
 function hideDiv(divId) {
     let x = document.getElementById(divId);
     if (x.style.display === "none") {
@@ -89,3 +174,12 @@ function hideDiv(divId) {
         x.style.display = "none";
     }
 }
+
+
+
+
+
+
+
+
+
